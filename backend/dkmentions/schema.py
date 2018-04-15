c17ad4aa13c2cd6168928f23bd3d28fc11b66f82
laveesingh <lavees96@gmail.com>
2018-04-15 17:48:49 +0530

    added partial mtuations

--- backend/dkmentions/schema.py
+++ backend/dkmentions/schema.py
@@ -1,3 +1,4 @@
+import json
 import graphene
 from graphene_django.types import DjangoObjectType
 from graphql_relay.node.node import from_global_id
@@ -22,3 +23,29 @@ class Query(graphene.AbstractType):
     def resolve_first_post(self, args=None, context=None, info=None):
         rid = from_global_id(args.get('id', 1))
         return models.FacebookPost.objects.get(pk=rid[1])
+
+
+# class CreateFacebookPostMutation(graphene.Mutation):
+
+    # class Input:
+        # content = graphene.String()
+
+    # status = graphene.Int()
+    # formErrors = graphene.String()
+    # content = graphene.Field(FacebookPostType)
+
+    # @staticmethod
+    # def mutate(root, args, context, info):
+        # if not context.user.is_authenticated():
+            # return CreateFacebookPostMutation(status=403)
+        # content = args.get('content', '').strip()
+        # if not content:
+            # return CreateFacebookPostMutation(status=400, formErrors=json.dumps({
+                # 'content': ['No content in Facebook Post']
+            # }))
+        # obj = models.FacebookPost.objects.create(content=content)
+        # return CreateFacebookPostMutation(status=200, content=obj)
+
+
+# class Mutation(graphene.AbstractType):
+    # create_content = CreateFacebookPostMutation.Field()
--- backend/dkmentions/tests/test_schema.py
+++ backend/dkmentions/tests/test_schema.py
@@ -1,4 +1,6 @@
 import pytest
+# from django.contrib.auth.models import AnonymousUser
+# from django.test import RequestFactory
 from mixer.backend.django import mixer
 from graphql_relay.node.node import to_global_id
 
@@ -15,14 +17,36 @@ def test_facebookpost_type():
 def test_resolve_all_posts():
     mixer.blend('dkmentions.FacebookPost')
     mixer.blend('dkmentions.FacebookPost')
-    q = schema.Query()
-    res = q.resolve_all_posts(None, None, None)
+    query = schema.Query()
+    res = query.resolve_all_posts(None, None, None)
     assert res.count() == 2, 'Should return all posts'
 
 
 def test_resolve_first_post():
     msg = mixer.blend('dkmentions.FacebookPost')
-    q = schema.Query()
+    query = schema.Query()
     _id = to_global_id('FacebookPostType', msg.pk)
-    res = q.resolve_first_post({'id': _id}, None, None)
+    res = query.resolve_first_post({'id': _id}, None, None)
     assert res == msg, 'Should return the first post'
+
+
+# def test_create_facebook_post_mutation():
+    # user = mixer.blend('auth.User')
+    # mutation = schema.CreateFacebookPostMutation()
+    # data = {
+        # 'content': 'TestContent'
+    # }
+    # request = RequestFactory().get('/')
+    # request.user = AnonymousUser()
+    # response = mutation.mutate(None, data, request, None)
+    # assert response.status == 403, 'Should return 403 if user is not logged in'
+
+    # request.user = user
+    # response = mutation.mutate(None, {}, req, None)
+    # assert response.status == 400, 'Should return 400 if there are form errors'
+    # assert 'content' in response.formErrors,\
+            # 'Should have form error for content field'
+
+    # response = mutation.mutate(None, data, request, None)
+    # assert response.status == 200, 'Should return 200 if everything is fine'
+    # assert response
