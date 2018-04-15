47d3f52298a0ee9a59987bd196dd17200f1dc299
laveesingh <lavees96@gmail.com>
2018-04-15 15:45:59 +0530

    successfully integrated graphql

---
+++ backend/backend/schema.py
@@ -0,0 +1,8 @@
+import graphene
+
+
+class Queries(graphene.ObjectType):
+    dummy = graphene.String()
+
+
+schema = graphene.Schema(query=Queries)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dkmentions',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'backend.schema.schema',
+}
